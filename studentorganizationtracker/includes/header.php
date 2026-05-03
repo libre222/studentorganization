a4287c61bf23ce6f72cd60694669c8f0aca56bc2
libre222 <libreliberty222@gmail.com>
2026-05-03 15:05:00 +0800

    header.php

--- studentorganizationtracker/includes/header.php
+++ studentorganizationtracker/includes/header.php
@@ -14,8 +14,14 @@ if (!isset($_SESSION['user_id'])) {
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>Student Org Tracker</title>
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link rel="stylesheet" href="/studentorganizationtracker/assets/css/style.css">
 </head>
 <body>
 
 <div class="layout">
+<?php include __DIR__ . '/sidebar.php'; ?>
+<div class="main-content">
+<?php include __DIR__ . '/navbar.php'; ?>
+<div class="page-content">
